7700f1042deefec8d92face2e00a8337ce499ee4
logotype <victor@logotype.se>
2013-02-22 11:32:45 +0800

    added commands

--- useful-unix-stuff.sh
+++ useful-unix-stuff.sh
@@ -1,5 +1,11 @@
-# Get IP address on command line (Ethernet 0):
+# Get IP address (Ethernet 0):
 /sbin/ifconfig eth0 | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}'
 
-# Get IP address on command line (All interfaces):
+# Get IP address (All interfaces):
 /sbin/ifconfig |grep -B1 "inet addr" |awk '{ if ( $1 == "inet" ) { print $2 } else if ( $2 == "Link" ) { printf "%s:" ,$1 } }' |awk -F: '{ print $1 ": " $3 }'
+
+# Find file by name and ignore errors
+find / -name "<name>" 2> /dev/null
+
+# Run the last command as root
+sudo !!
